5bdbf377d136215bc05d07bcd36ff921b5d02658
sagar <rsaagar7200@gmail.com>
2023-01-09 15:46:20 +0545

    text input

--- src/pages/Firstpage.jsx
+++ src/pages/Firstpage.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import "../styles/firstpage.css"
-import {Grid,Box,TextField  } from '@mui/material';
-import flower from '../images/flower.jpg'
+import React from "react";
+import "../styles/firstpage.css";
+import { Grid, Box } from "@mui/material";
+import flower from "../images/flower.jpg";
 
 export const Firstpage = () => {
   return (
-    <div className='wrapper'>
-    <Grid container spacing={0}>
-    <Grid item xs={6} className="wrapper-grid1">
-    <Box className="box-grid" >
-     <p className="grid-question"> Hello, what's your name?</p>
-     <TextField  label="Standard" variant="standard" color="primary" fullWidth  />
-     
-    </Box>
-    </Grid>
-    <Grid item xs={6} className="images">
-    <img src={flower} alt="flower"/>
-    </Grid>
-   
-  </Grid>
-  </div>
-  )
-}
+    <div className="wrapper">
+      <Grid container spacing={0}>
+        <Grid item xs={6} className="wrapper-grid1">
+          <Box className="box-grid">
+            <p className="grid-question"> Hello, what's your name?</p>
+            <input
+              type="text"
+              className="answer_input"
+              placeholder="Type your answer here..."
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6} className="images">
+          <img src={flower} alt="flower" />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
