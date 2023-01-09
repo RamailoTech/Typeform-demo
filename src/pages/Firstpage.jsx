8a5cad8fcc3bed4a6ee616198915dfead01c1792
karina38 <karina@ramailo.tech>
2023-01-09 15:47:33 +0545

    ui

--- src/pages/Firstpage.jsx
+++ src/pages/Firstpage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../styles/firstpage.css"
-import {Grid,Box,TextField  } from '@mui/material';
+import {Grid,Box,TextField,Button  } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
 import flower from '../images/flower.jpg'
 
 export const Firstpage = () => {
@@ -11,11 +12,16 @@ export const Firstpage = () => {
     <Box className="box-grid" >
      <p className="grid-question"> Hello, what's your name?</p>
      <TextField  label="Standard" variant="standard" color="primary" fullWidth  />
+     <Button variant="contained" sx={{marginTop:"15px"}} >OK</Button>
+     
      
     </Box>
     </Grid>
+    
     <Grid item xs={6} className="images">
     <img src={flower} alt="flower"/>
+    <Button variant="contained" className='second-button'>Powered by typeform</Button>
+
     </Grid>
    
   </Grid>
