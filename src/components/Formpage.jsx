6abab7818029539a9fbe9c5205c34babea0f12d2
sagar <rsaagar7200@gmail.com>
2023-01-10 15:29:03 +0545

    button clivk logic

--- src/components/Formpage.jsx
+++ src/components/Formpage.jsx
@@ -1,15 +1,15 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { Grid, Box, Button } from "@mui/material";
 import flower from "../assets/images/flower.jpg";
 import CheckIcon from "@mui/icons-material/Check";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import {TextInput} from "../components/input/textInput/inputs"
-import {RadioInput} from "../components/input/radioInput/RadioInput";
+import { TextInput } from "../components/input/textInput/inputs";
+import { RadioInput } from "../components/input/radioInput/RadioInput";
 import MultipleChoice from "../components/input/multipleChoice/MultipleChoice";
 import Autocomplete from "../components/input/autoComplete/autoCompleteInput";
 export const Formpage = ({ items }) => {
-  const[pages, SetPages]=useState(false)
- 
+  const [pages, SetPages] = useState(false);
+
   return (
     <div className={`wrapper ${pages && `translatepage`}`}>
       <Grid container spacing={0}>
@@ -20,14 +20,20 @@ export const Formpage = ({ items }) => {
               <ArrowForwardIcon color="primary" sx={{ fontSize: "14px" }} />
               {items.question}
             </p>
-            {
-              items.answer.type==="text"?<TextInput/>:items.answer.type==="radio"?<RadioInput options={items.answer.options}/>:items.answer.type==="dropdown"?<Autocomplete/>:<MultipleChoice options={items.answer.options}/>
-            }
-          
-           <div>
-            <Button variant="contained" endIcon={<CheckIcon />}>
-              OK
-            </Button>
+            {items.answer.type === "text" ? (
+              <TextInput />
+            ) : items.answer.type === "radio" ? (
+              <RadioInput options={items.answer.options} />
+            ) : items.answer.type === "dropdown" ? (
+              <Autocomplete options={items.answer.options} />
+            ) : (
+              <MultipleChoice options={items.answer.options} />
+            )}
+
+            <div>
+              <Button variant="contained" endIcon={<CheckIcon />}>
+                OK
+              </Button>
             </div>
           </Box>
         </Grid>
@@ -36,8 +42,12 @@ export const Formpage = ({ items }) => {
           <Button variant="contained" className="btn-banner1">
             Powered by Typeform
           </Button>
-          <Button variant="contained" className="btn-banner2" endIcon={<CheckIcon />} onClick={()=>SetPages(true)}>
-          </Button>
+          <Button
+            variant="contained"
+            className="btn-banner2"
+            endIcon={<CheckIcon />}
+            onClick={() => SetPages(true)}
+          ></Button>
         </Grid>
       </Grid>
     </div>
