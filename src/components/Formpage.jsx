f86f60845d0c74606c9dddedbbf956af1845862b
sagar <rsaagar7200@gmail.com>
2023-01-11 17:27:41 +0545

    conditional question rendering

--- src/components/Formpage.jsx
+++ src/components/Formpage.jsx
@@ -7,15 +7,16 @@ import { TextInput } from "../components/input/textInput/inputs";
 import { RadioInput } from "../components/input/radioInput/RadioInput";
 import MultipleChoice from "../components/input/multipleChoice/MultipleChoice";
 import Autocomplete from "../components/input/autoComplete/autoCompleteInput";
-import {DateInput} from "../components/input/dateInput";
+import { DateInput } from "../components/input/dateInput";
 import FormContext from "../context/form/FormContext";
 import QuestionContext from "../context/questions/QuestionContext";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 export const Formpage = ({ items, activePage, pageno }) => {
-  const questions = useContext(QuestionContext)
-  const { page, setPage } = useContext(FormContext)
+  const questions = useContext(QuestionContext);
+  const { page, setPage, visiblePageNumber, setVisiblePageNumber } =
+    useContext(FormContext);
+
   return (
     <div
       style={{ transform: `translateY(-${activePage * 100}%)` }}
@@ -37,24 +38,41 @@ export const Formpage = ({ items, activePage, pageno }) => {
                 options={items.answer.options}
               />
             ) : items.answer.type === "dropdown" ? (
-              <Autocomplete  question={items.question} options={items.answer.options} />
+              <Autocomplete
+                question={items.question}
+                options={items.answer.options}
+              />
             ) : items.answer.type === "dateInput" ? (
-              <DateInput  question={items.question}/>
-            ): (
-              <MultipleChoice options={items.answer.options}  question={items.question} />
+              <DateInput question={items.question} />
+            ) : (
+              <MultipleChoice
+                options={items.answer.options}
+                question={items.question}
+              />
             )}
 
             <div>
-              {
-                (page===questions.length-1)?(
-                 <Link to="/typeform/result" ><Button variant="contained" className="grid-button">Submit</Button></Link>
-                ):
-                <Button variant="contained" className="grid-button" endIcon={<CheckIcon />}   
-                onClick={() => setPage(Math.min(page + 1, questions.length - 1))}>
+              {page === questions.length - 1 ? (
+                <Link to="/typeform/result">
+                  <Button variant="contained" className="grid-button">
+                    Submit
+                  </Button>
+                </Link>
+              ) : (
+                <Button
+                  variant="contained"
+                  className="grid-button"
+                  endIcon={<CheckIcon />}
+                  onClick={() => {
+                    setPage(Math.min(page + 1, questions.length - 1));
+                    setVisiblePageNumber(
+                      Math.min(visiblePageNumber + 1, questions.length - 1)
+                    );
+                  }}
+                >
                   OK
                 </Button>
-              }
-             
+              )}
             </div>
           </Box>
         </Grid>
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,20 +1,53 @@
-import React, { useContext } from "react"
-import QuestionContext from "../context/questions/QuestionContext"
-import { Button, ButtonGroup } from "@mui/material"
-import { Formpage } from "../components/Formpage"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
-import ExpandLessIcon from "@mui/icons-material/ExpandLess"
-import FormContext from "../context/form/FormContext"
-import "../assets/styles/form.css"
+import React, { useContext, useEffect } from "react";
+import QuestionContext from "../context/questions/QuestionContext";
+import { Button, ButtonGroup } from "@mui/material";
+import { Formpage } from "../components/Formpage";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import FormContext from "../context/form/FormContext";
+import "../assets/styles/form.css";
 
 export const Form = () => {
-  const questions = useContext(QuestionContext)
-  const { page, setPage } = useContext(FormContext)
+  let questions = useContext(QuestionContext);
+  const {
+    page,
+    setPage,
+    formValue,
+    visiblePageNumber,
+    setVisiblePageNumber,
+    setPageLength,
+    pageLength,
+  } = useContext(FormContext);
 
   return (
     <div className={`wrapper`}>
-      {questions.map((items, index) => {
-        return <Formpage items={items} pageno={index} activePage={page} />
+      {questions.map((question, index) => {
+        if (question.parentId !== undefined) {
+          let parentQuestion = questions.find(
+            (item) => item.id === question.parentId
+          );
+          if (formValue[parentQuestion.question] === question.parent) {
+            setPageLength(pageLength + 1);
+            return (
+              <Formpage
+                items={question}
+                pageno={visiblePageNumber}
+                activePage={page}
+              />
+            );
+          }
+        } else {
+          setPageLength(pageLength + 1);
+
+          return (
+            <Formpage
+              items={question}
+              pageno={visiblePageNumber}
+              activePage={page}
+            />
+          );
+        }
+        return <></>;
       })}
       <div className="navigation">
         <ButtonGroup
@@ -22,25 +55,33 @@ export const Form = () => {
           aria-label="outlined primary button group"
         >
           <Button
-            onClick={() => setPage(Math.max(page - 1, 0))}
+            onClick={() => {
+              setPage(Math.max(page - 1, 0));
+              setVisiblePageNumber(Math.max(visiblePageNumber - 1, 0));
+            }}
             disabled={page === 0}
-            sx={{backgroundColor:"#0445af"}}
+            sx={{ backgroundColor: "#0445af" }}
           >
             <ExpandLessIcon />
           </Button>
           <Button
-            onClick={() => setPage(Math.min(page + 1, questions.length - 1))}
+            onClick={() => {
+              if (page <= questions.length - 1) {
+                setPage(Math.min(page + 1, questions.length - 1));
+                setVisiblePageNumber(visiblePageNumber + 1);
+              }
+            }}
             disabled={page === questions.length - 1}
-            sx={{backgroundColor:"#0445af"}}
+            sx={{ backgroundColor: "#0445af" }}
           >
             <ExpandMoreIcon />
           </Button>
         </ButtonGroup>
 
-        <Button variant="contained"  sx={{backgroundColor:"#0445af "}}>
+        <Button variant="contained" sx={{ backgroundColor: "#0445af " }}>
           Powered by Ramailo.tech
         </Button>
       </div>
     </div>
-  )
-}
+  );
+};
